4921f6f2750f23eff6442579f46eeee4844ba846
Ivan Chensky <i.chensky@ameria.de>
2018-05-17 17:16:50 +0300

    add domains, that contiains adds

--- src/AddDomains/AddDomains/Program.cs
+++ src/AddDomains/AddDomains/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AddDomains
 {
+    public class Info {
+        public string Url { get; set; }
+        public string Hash { get; set; }
+    }
     class Program
     {
        
@@ -19,6 +26,14 @@ namespace AddDomains
         public static async Task func() {
 
             var path = "../../../../../../";
+            var info = new List<Info>();
+            var dirPath = Path.Combine(path, "result");
+            var domainsPath = Path.Combine(dirPath,"domains.txt");
+            var infoPath = Path.Combine(dirPath, "info.txt");
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
             var list = File.ReadAllLines($"{path}urls.txt");
 
             var urls = new List<string>();
@@ -31,143 +46,167 @@ namespace AddDomains
                 }
             }
 
-            var domains = await DomainsFromUrls(urls);
-            File.WriteAllLines($"{path}domains.txt", domains);
-        }
-        public static async Task<List<string>> DomainsFromUrls(IEnumerable<string> urls) {
+            await Task.WhenAll(urls.Select(x=> {
+                var hash = GetHashString(x);
+                var filename = $"{hash}.txt";
+                info.Add(new Info { Hash=hash, Url=x });
+                Console.WriteLine($"Downloading: {hash}={x}");
+
+                return Download(x, Path.Combine(dirPath, filename)); }));
+
+            File.WriteAllText(infoPath, JsonConvert.SerializeObject(info));
+
 
             var domains = new List<string>();
-            foreach (var url in urls)
-            {
-                try
-                {
-                    Console.WriteLine($"Getting domain names from url: {url}");
-                    var client = new HttpClient() {
+            var files = Directory.GetFiles(dirPath);
 
-                    };
-                    var domainsStr = await client.GetStringAsync(url);
-                    var domainsArr = domainsStr.Split(new[] { '\r', '\n' });
-                    var xDomains = await Domains(domainsArr);
-                    domains.AddRange(xDomains);
-                }
-                catch (Exception ex)
+            foreach (var item in files)
+            {
+                var fileName = new FileInfo(item).Name;
+                var hash = fileName.Replace(".txt","");
+                var infoItem = info.FirstOrDefault(x => x.Hash == hash);
+
+                if (infoItem != null)
                 {
-                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine($"Processing: {hash}={infoItem.Url}");
+
+                    var lines = File.ReadAllLines(item);
+                    var fileDomains = Domains(lines);
+                    domains.AddRange(fileDomains);
                 }
             }
-            var arr = domains.Distinct().OrderBy(x=>x);
 
+            Console.WriteLine($"Saving domains ...");
 
+            File.WriteAllLines(domainsPath, domains.OrderBy(x=>x).Distinct());
+        }
+        public static async Task Download(string url, string filePath) {
 
-            return domains;
+            var client = new HttpClient();
+            var domainsStr = await client.GetStringAsync(url);
+
+            await File.WriteAllTextAsync(filePath, domainsStr);
         }
 
-        public static Task<List<string>> Domains(IEnumerable<string> domains)
+
+
+
+        public static List<string> Domains(IEnumerable<string> domains)
         {
-            return Task.Factory.StartNew(() =>
+
+            var list = new List<string>();
+            foreach (var item in domains)
             {
-                var list = new List<string>();
-                foreach (var item in domains)
+                var str = item.Trim();
+                var index = str.IndexOf("#");
+                if (index > -1)
                 {
-                    var str = item;
-                    var index = str.IndexOf("#");
-                    if (index > -1)
-                    {
-                        str = str.Remove(index);
-                    }
-                    index = str.IndexOf(";");
-                    if (index > -1)
-                    {
-                        str = str.Remove(index);
-                    }
-                    index = str.IndexOf(",");
-                    if (index > -1)
-                    {
-                        str = str.Remove(index);
-                    }
-                    
-
-                  
-                    str = str
-                        .Replace('|', ' ')
-                        .Replace('^', ' ')
-                        .Replace('\t', ' ')
-                        .Trim();
-                    ;
-                    str = Regex.Replace(str, @"\s+", " ");
-
-                    var aa = str.Split(' ');
-
-                    string domain = null;
-
-                    if (aa.Length == 1)
-                    {
-                        domain = str;
-                    }
-                    else if (aa.Length == 2)
-                    {
-                        domain = aa[1];
-                    }
-                    else if (aa.Length == 0) {
-                    }
-                    else
-                    {
-                        //Console.WriteLine($"Error in domain str: {str}");
-                    }
-                    if (domain==null||domain.Length==0)
-                    {
-                        continue;
-                    }
-
-                        index = domain.IndexOf("www.");
-                        if (index == 0)
-                        {
-                            domain = domain.Remove(0, 4);
-                        }
-
-                    index = domain.IndexOf("http://");
-                    if (index == 0)
-                    {
-                        domain = domain.Remove(0, 7);
-                    }
-                    index = domain.IndexOf("https://");
-                    if (index == 0)
-                    {
-                        domain = domain.Remove(0, 8);
-                    }
-
-
-
-
-                    if (domain != null && domain.Length > 0
-                        && domain != "localhost"
-                        && domain != "localhost.localdomain"
-                        && domain != "broadcasthost"
-                        && domain != "local"
-                        && domain != "ip6-localnet"
-                        && domain != "ip6-mcastprefix"
-                        && domain != "ip6-allnodes"
-                        && domain != "ip6-allrouters"
-                        && domain != "ip6-allhosts"
-                        && domain != "0.0.0.0"
-                        && domain != "127.0.0.1"
-                        && Regex.Match(domain[0].ToString(), "[a-zA-Z0-9]").Success
-
-                        &&domain.LastIndexOf(".jpg")!=domain.Length-4
-                        &&domain.LastIndexOf(".gif")!=domain.Length-4
-                        &&domain.LastIndexOf(".png")!=domain.Length-4
-
-
-                        )
-                    {
-                        list.Add(domain);
-
-                    }
+                    str = str.Remove(index);
                 }
-                return list;
 
-            });
-            
+                index = str.IndexOf(";");
+                if (index > -1)
+                {
+                    str = str.Remove(index);
+                }
+
+                if (str.IndexOf("||") == 0 && str.IndexOf("^") == str.Length - 1)
+                {
+                    str = str.Remove(str.Length - 1).Remove(0, 2);
+                }
+                if (str.Length == 0 || !Regex.Match(str[0].ToString(), "[0-9a-zA-Z]").Success)
+                {
+                    continue;
+                }
+                str = Regex.Replace(str, @"\s+", " ");
+
+
+
+
+
+                var arr = str.Split(' ');
+
+                string domain = null;
+
+                if (arr.Length == 1)
+                {
+                    domain = str;
+                }
+                else if (arr.Length == 2)
+                {
+                    domain = arr[1];
+                }
+                else
+                {
+                    continue;
+                }
+                if (domain.Length == 0)
+                {
+                    continue;
+                }
+                UriBuilder uri = null;
+                try
+                {
+                    uri = new UriBuilder(domain);
+
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+
+                var host = uri.Host;
+
+                if (System.Net.IPAddress.TryParse(host, out var ip))
+                {
+                    continue;
+                }
+
+
+
+
+
+                index = host.IndexOf("www.");
+                if (index == 0)
+                {
+                    host = host.Remove(0, 4);
+                }
+
+
+
+
+
+                if (host.Length > 0
+                    && host.IndexOf(".") > 0
+                    && host.IndexOf(".") != host.Length - 1
+                    && host.IndexOf(":") == -1
+                    && host != "localhost.localdomain"
+                    )
+                {
+                    list.Add(host);
+
+                }
+            }
+            return list;
+
+
+        }
+
+
+        public static byte[] GetHash(string inputString)
+        {
+            var algorithm = SHA256.Create();  //or use SHA256.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
         }
     }
 }
